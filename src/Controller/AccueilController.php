7ec1e958400d02091b9b4ba982ad4992fa571176
devJeremie <chabanaisjeremie@gmail.com>
2024-01-21 18:48:47 +0100

    DB-Entity

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -9,9 +9,19 @@ use Symfony\Component\Routing\Annotation\Route;
 class AccueilController extends AbstractController
 {
     /**
-     * @Route("/accueil", name="app_accueil")
+     * @Route("/accueil", name="app_accueil, methods={"GET"})
      */
     public function index(): Response
+    {
+        return $this->render('accueil/index.html.twig', [
+            'controller_name' => 'AccueilController',
+        ]);
+    }
+    /*création d'une seconde route vers le meme fichier*/
+     /**
+     * @Route("/contact", name="app_contact")
+     */
+    public function contact(): Response
     {
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
