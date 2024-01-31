4e69ac7e6ab18340c9f7bf5bc9c8230f3d8a0715
devJeremie <chabanaisjeremie@gmail.com>
2024-01-31 09:08:24 +0100

    ContactVue

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Contact;
+use App\Repository\ContactRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,10 +13,10 @@ class ContactController extends AbstractController
     /**
      * @Route("/contacts", name="contacts")
      */
-    public function listeContact(): Response
+    public function listeContact(ContactRepository $repo): Response
     {
-        $manager = $this->getDoctrine()->getManager();
-        $repo =  $manager->getRepository(Contact::class);
+        #{$manager = $this->getDoctrine()->getManager();#}
+        #{$repo =  $manager->getRepository(Contact::class);#}
         $contacts = $repo->findAll();
 
         return $this->render('contact/listeContacts.html.twig', [
