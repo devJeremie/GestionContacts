8e53bcb3adde2a712355ffa75c1fdf7b71c25c5a
devJeremie <chabanaisjeremie@gmail.com>
2024-02-14 14:16:03 +0100

    MajCategoryFixtures

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Contact;
+use App\Repository\CategorieRepository;
 use App\Repository\ContactRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,13 +14,13 @@ class ContactController extends AbstractController
     /**
      * @Route("/contacts", name="contacts", methods={"GET"})
      */
-    public function listeContacts(ContactRepository $repo): Response
+    public function listeContacts(ContactRepository $repo ): Response
     {
         #{$manager = $this->getDoctrine()->getManager();#}
         #{$repo =  $manager->getRepository(Contact::class);#}
         $Contacts = $repo->findAll();
+        
 
-       
         return $this->render('contact/listeContacts.html.twig', [
            /* 'controller_name' => 'ContactController',*/
            'lesContacts' => $Contacts
@@ -27,7 +28,7 @@ class ContactController extends AbstractController
     }
 
      /**
-     * @Route("/contact/{id}", name="ficheContact", methods={"GET, POST"})
+     * @Route("/contact/{id}", name="ficheContact", methods={"GET"})
      */
     public function ficheContact( $id, ContactRepository $repo): Response
     {
